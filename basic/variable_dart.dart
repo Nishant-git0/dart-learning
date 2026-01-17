9580ecb40689fe76cced435d2e37600b360a949a
Nishant <nishant.krthakur111@gmail.com>
2026-01-18 02:37:01 +0545

    2 question of variable is done

---
+++ basic/variable_dart.dart
@@ -0,0 +1,14 @@
+//Q1.Declare a variable and assign an integer value. Change it later and print both values.
+/*void main() {
+  var number = 10;
+  print("first variable value: $number ");
+  number = 20;
+  print("second variable value: $number ");
+}*/
+
+//Q2.Store a user’s name and age in variables and print a formatted sentence.
+void main() {
+  String name = "Nishant ";
+  int age = 32;
+  print("my name is $name and my age is $age");
+}
